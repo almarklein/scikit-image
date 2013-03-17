39fc874daeba326f8077b130757547663342af58
Josh Warner (Mac) <warner.joshua@mayo.edu>
2013-03-17 08:48:32 -0500

    rename files to conform with naming conventions

--- skimage/measure/__init__.py
+++ skimage/measure/__init__.py
@@ -1,5 +1,5 @@
 from .find_contours import find_contours
-from .marching_cubes import marching_cubes, mesh_surface_area
+from ._marching_cubes import marching_cubes, mesh_surface_area
 from ._regionprops import regionprops, perimeter
 from ._structural_similarity import structural_similarity
 from ._polygon import approximate_polygon, subdivide_polygon
